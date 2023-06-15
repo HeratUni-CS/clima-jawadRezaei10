8b64d11bfb4544932e5a66c5f3999fe01e2e92cc
Jawad Rezaei <107846753+jawadRezaei10@users.noreply.github.com>
2023-06-15 10:12:59 +0430

    Update loading_screen.dart

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,20 +1,36 @@
 import 'package:flutter/material.dart';
+import 'location_screen.dart';
+import 'package:clima/services/weather.dart';
 
 class LoadingScreen extends StatefulWidget {
+  const LoadingScreen({super.key});
+
   @override
-  _LoadingScreenState createState() => _LoadingScreenState();
+  State<StatefulWidget> createState() {
+    return _LoadingScreenState();
+  }
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  void getLocationData() async {
+    var weatherData = await WeatherModel().getLocationWeather();
+    if (!mounted) {}
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen(
+        locationWeather: weatherData,
+      );
+    }));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: TextButton(
+        child: ElevatedButton(
           onPressed: () {
-            //Get the current location
+            getLocationData();
           },
-          child: Text('Get Location'),
+          child: const Text('Get Location'),
         ),
       ),
     );
