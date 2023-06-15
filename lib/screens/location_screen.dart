480684bc84ada84db5e432bdcd07651e8348e062
Jawad Rezaei <107846753+jawadRezaei10@users.noreply.github.com>
2023-06-15 10:10:14 +0430

    Update location_screen.dart

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -1,70 +1,61 @@
+import 'package:clima/widgets/icon_button_widgets/icon_button_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:clima/utilities/constants.dart';
 
-class LocationScreen extends StatefulWidget {
+class CityScreen extends StatefulWidget {
+  const CityScreen({super.key});
+
+  static const String id = '/city_screen';
+
   @override
-  _LocationScreenState createState() => _LocationScreenState();
+  State<CityScreen> createState() => _CityScreenState();
 }
 
-class _LocationScreenState extends State<LocationScreen> {
+class _CityScreenState extends State<CityScreen> {
+  late String cityName;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        decoration: BoxDecoration(
+        decoration: const BoxDecoration(
           image: DecorationImage(
-            image: AssetImage('images/location_background.jpg'),
+            image: AssetImage('images/city_background.jpg'),
             fit: BoxFit.cover,
-            colorFilter: ColorFilter.mode(
-                Colors.white.withOpacity(0.8), BlendMode.dstATop),
           ),
         ),
-        constraints: BoxConstraints.expand(),
         child: SafeArea(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  TextButton(
-                    onPressed: () {},
-                    child: Icon(
-                      Icons.near_me,
-                      size: 50.0,
-                    ),
-                  ),
-                  TextButton(
-                    onPressed: () {},
-                    child: Icon(
-                      Icons.location_city,
-                      size: 50.0,
-                    ),
-                  ),
-                ],
-              ),
-              Padding(
-                padding: EdgeInsets.only(left: 15.0),
-                child: Row(
-                  children: <Widget>[
-                    Text(
-                      '32°',
-                      style: kTempTextStyle,
-                    ),
-                    Text(
-                      '☀️',
-                      style: kConditionTextStyle,
-                    ),
-                  ],
+            children: [
+              Align(
+                alignment: Alignment.topLeft,
+                child: IconButtonWidget(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: Icons.arrow_back_ios,
                 ),
               ),
-              Padding(
-                padding: EdgeInsets.only(right: 15.0),
-                child: Text(
-                  "It's 🍦 time in San Francisco!",
-                  textAlign: TextAlign.right,
-                  style: kMessageTextStyle,
+              Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+                child: TextField(
+                  style: const TextStyle(
+                    color: Colors.black,
+                  ),
+                  decoration: kTextFieldInputDecoration,
+                  onChanged: (value) {
+                    cityName = value;
+                  },
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context, cityName);
+                },
+                child: const Text(
+                  'Get Weather',
+                  style: kButtonTextStyle,
                 ),
               ),
             ],
